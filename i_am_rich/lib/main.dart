17dbe53078448e53b4fcf16413c48eb4ea2f3886
snlucas <lucas.lambda.101@gmail.com>
2020-12-23 15:29:58 -0300

    Initial Scaffold App

---
+++ i_am_rich/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+          home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Center(
+            child: Text('I am Rich'),
+          ),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+      )),
+    );
