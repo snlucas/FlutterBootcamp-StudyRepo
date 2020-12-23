3da50ee8e4006f9d169c263f19e80599b84eb9ca
snlucas <lucas.lambda.101@gmail.com>
2020-12-23 16:28:58 -0300

    Update app image to show a diamond on the screen

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -2,13 +2,19 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-          home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-        appBar: AppBar(
-          title: Center(
-            child: Text('I am Rich'),
+        home: Scaffold(
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar(
+            title: Center(
+              child: Text('I am Rich'),
+            ),
+            backgroundColor: Colors.blueGrey[900],
           ),
-          backgroundColor: Colors.blueGrey[900],
+          body: Center(
+            child: Image(
+              image: AssetImage('images/diamond.png'),
+            ),
+          ),
         ),
-      )),
+      ),
     );
