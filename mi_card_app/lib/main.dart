134a70dde093238a17574a48972acd524c3ff766
snlucas <lucas.lambda.101@gmail.com>
2020-12-28 14:21:38 -0300

    Add Profile Picture and Profile Name

--- mi_card_app/lib/main.dart
+++ mi_card_app/lib/main.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-          backgroundColor: Colors.teal,
-          body: Container(),
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Container(
+            child: Column(
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/Perfil01.jpeg'),
+                ),
+                Text(
+                  'Lucas Nogueira',
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
+  }
+}
