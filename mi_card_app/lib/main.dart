beb4c38e7bfd861aacf49429fe3c190d6b39e89e
snlucas <lucas.lambda.101@gmail.com>
2020-12-28 18:09:18 -0300

    add SourceSansPro font and contact info

--- mi_card_app/lib/main.dart
+++ mi_card_app/lib/main.dart
@@ -10,7 +10,9 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Container(
+            width: double.infinity,
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 CircleAvatar(
                   radius: 50.0,
@@ -19,8 +21,62 @@ class MyApp extends StatelessWidget {
                 Text(
                   'Lucas Nogueira',
                   style: TextStyle(
+                    fontFamily: 'Pacifico',
                     fontSize: 40.0,
                     color: Colors.white,
+                  ),
+                ),
+                Text(
+                  'FLUTTER DEVELOPER',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'SourceSans',
+                    fontSize: 20.0,
+                  ),
+                ),
+                Divider(
+                  indent: 150.0,
+                  endIndent: 150.0,
+                  color: Colors.white,
+                ),
+                Container(
+                  height: 50.0,
+                  padding: EdgeInsets.all(10.0),
+                  margin: EdgeInsets.all(20.0),
+                  color: Colors.white,
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.phone,
+                        color: Colors.teal,
+                      ),
+                      Text(
+                        '+55 (51) 99999-9999',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  height: 50.0,
+                  padding: EdgeInsets.all(10.0),
+                  margin: EdgeInsets.all(20.0),
+                  color: Colors.white,
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.mail,
+                        color: Colors.teal,
+                      ),
+                      Text(
+                        'lucas.mail@mail.com',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ],
