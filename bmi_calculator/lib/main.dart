2e32a0548370e20435c8507439cc3f2598919f56
snlucas <lucas.lambda.101@gmail.com>
2021-01-12 16:39:46 -0300

    Initial theme modifications

--- bmi_calculator/lib/main.dart
+++ bmi_calculator/lib/main.dart
@@ -6,7 +6,10 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark(),
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+      ),
       home: InputPage(),
     );
   }
@@ -30,6 +33,7 @@ class _InputPageState extends State<InputPage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
+        onPressed: () {},
         child: Icon(Icons.add),
       ),
     );
